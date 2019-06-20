a5392e4c2556f3063863a2a96caacb2fa97cb505
Jordan Gaeta <jordangaeta@Jordans-MacBook-Pro.local>
2019-06-20 15:20:06 -0400

    update again

---
+++ resources/screens/11ff575b-cdf7-49f6-9ad0-085b905959f2-1561058378129.js
@@ -0,0 +1,98 @@
+jQuery("#simulation")
+  .on("click", ".s-11ff575b-cdf7-49f6-9ad0-085b905959f2 .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Rectangle_8")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/a387b1a7-16a1-40f6-b9b7-d74f3cb979f9"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Rectangle_3")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/dcbb8269-0125-4602-83e7-e21c1ffcaa8d"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  })
+  .on("mouseenter dragenter", ".s-11ff575b-cdf7-49f6-9ad0-085b905959f2 .mouseenter", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getDirectEventFirer(this);
+    if(jFirer.is("#s-Rectangle_3") && jFirer.has(event.relatedTarget).length === 0) {
+      event.backupState = true;
+      event.target = jFirer;
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimChangeStyle",
+                  "parameter": [ {
+                    "#s-11ff575b-cdf7-49f6-9ad0-085b905959f2 #s-Rectangle_3 span": {
+                      "attributes": {
+                        "color": "#FFDF00"
+                      }
+                    }
+                  } ],
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    }
+  })
+  .on("mouseleave dragleave", ".s-11ff575b-cdf7-49f6-9ad0-085b905959f2 .mouseleave", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getDirectEventFirer(this);
+    if(jFirer.is("#s-Rectangle_3")) {
+      jEvent.undoCases(jFirer);
+    }
+  });
