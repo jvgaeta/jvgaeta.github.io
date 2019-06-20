9a7f9252eb56ed6286abef3740879986f77c05c6
Jordan Gaeta <jordangaeta@Jordans-MacBook-Pro.local>
2019-06-20 11:00:27 -0400

    testing wireframe for web app

---
+++ resources/scenarios/function-jim-links1561041939081.js
@@ -0,0 +1,63 @@
+(function(window, undefined) {
+
+  var jimLinks = {
+    "0fd709cc-1007-4703-a447-df1b52541925" : {
+      "Image_18" : [
+        "11ff575b-cdf7-49f6-9ad0-085b905959f2"
+      ]
+    },
+    "d12245cc-1680-458d-89dd-4f0d7fb22724" : {
+      "Rectangle_2" : [
+        "83c8c710-37f6-459b-8e72-06796434a738"
+      ],
+      "Rectangle_3" : [
+        "11ff575b-cdf7-49f6-9ad0-085b905959f2"
+      ]
+    },
+    "b59a2066-f3b7-46a0-ae64-38d093a9f115" : {
+      "Rectangle_4" : [
+        "dcbb8269-0125-4602-83e7-e21c1ffcaa8d"
+      ],
+      "Rectangle_6" : [
+        "83c8c710-37f6-459b-8e72-06796434a738"
+      ],
+      "Rectangle_2" : [
+        "83c8c710-37f6-459b-8e72-06796434a738"
+      ],
+      "Rectangle_3" : [
+        "11ff575b-cdf7-49f6-9ad0-085b905959f2"
+      ]
+    },
+    "dcbb8269-0125-4602-83e7-e21c1ffcaa8d" : {
+      "Rectangle_2" : [
+        "83c8c710-37f6-459b-8e72-06796434a738"
+      ],
+      "Rectangle_3" : [
+        "11ff575b-cdf7-49f6-9ad0-085b905959f2"
+      ],
+      "Rectangle_4" : [
+        "11ff575b-cdf7-49f6-9ad0-085b905959f2"
+      ],
+      "Rectangle_5" : [
+        "b59a2066-f3b7-46a0-ae64-38d093a9f115"
+      ]
+    },
+    "83c8c710-37f6-459b-8e72-06796434a738" : {
+    },
+    "a387b1a7-16a1-40f6-b9b7-d74f3cb979f9" : {
+      "Rectangle_6" : [
+        "0fd709cc-1007-4703-a447-df1b52541925"
+      ]
+    },
+    "11ff575b-cdf7-49f6-9ad0-085b905959f2" : {
+      "Rectangle_8" : [
+        "a387b1a7-16a1-40f6-b9b7-d74f3cb979f9"
+      ],
+      "Rectangle_3" : [
+        "dcbb8269-0125-4602-83e7-e21c1ffcaa8d"
+      ]
+    }    
+  }
+
+  window.jimLinks = jimLinks;
+})(window);
---
+++ resources/screens/dcbb8269-0125-4602-83e7-e21c1ffcaa8d-1561041939081.js
@@ -0,0 +1,100 @@
+jQuery("#simulation")
+  .on("click", ".s-dcbb8269-0125-4602-83e7-e21c1ffcaa8d .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Rectangle_2")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/83c8c710-37f6-459b-8e72-06796434a738"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Rectangle_3")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/11ff575b-cdf7-49f6-9ad0-085b905959f2"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Rectangle_4")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/11ff575b-cdf7-49f6-9ad0-085b905959f2"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Rectangle_5")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/b59a2066-f3b7-46a0-ae64-38d093a9f115"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  });
