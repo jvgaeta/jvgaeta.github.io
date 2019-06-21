2c78a13bb5b053a62f1a2bc46a48a8618f728699
Jordan Gaeta <jordangaeta@Jordans-MBP.lan1>
2019-06-20 23:06:15 -0400

    finished

---
+++ resources/screens/83c8c710-37f6-459b-8e72-06796434a738-1561086335008.js
@@ -0,0 +1,569 @@
+jQuery("#simulation")
+  .on("click", ".s-83c8c710-37f6-459b-8e72-06796434a738 .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Rectangle_5")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimSetValue",
+                  "parameter": {
+                    "target": [ "#s-Input_2" ],
+                    "value": {
+                      "datatype": "property",
+                      "target": "#s-Rectangle_5",
+                      "property": "jimGetValue"
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimHide",
+                  "parameter": {
+                    "target": [ "#s-Options_1" ],
+                    "effect": {
+                      "type": "slide",
+                      "easing": "swing",
+                      "duration": 500,
+                      "direction": "up"
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimRotate",
+                  "parameter": {
+                    "target": [ "#s-arrow_1" ],
+                    "angle": {
+                      "type": "rotateby",
+                      "value": "180"
+                    },
+                    "effect": {
+                      "type": "none",
+                      "easing": "swing",
+                      "duration": 400
+                    }
+                  },
+                  "exectype": "parallel",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Rectangle_6")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimSetValue",
+                  "parameter": {
+                    "target": [ "#s-Input_2" ],
+                    "value": {
+                      "datatype": "property",
+                      "target": "#s-Rectangle_6",
+                      "property": "jimGetValue"
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimHide",
+                  "parameter": {
+                    "target": [ "#s-Options_1" ],
+                    "effect": {
+                      "type": "slide",
+                      "easing": "swing",
+                      "duration": 500,
+                      "direction": "up"
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimRotate",
+                  "parameter": {
+                    "target": [ "#s-arrow_1" ],
+                    "angle": {
+                      "type": "rotateby",
+                      "value": "180"
+                    },
+                    "effect": {
+                      "type": "none",
+                      "easing": "swing",
+                      "duration": 400
+                    }
+                  },
+                  "exectype": "parallel",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Rectangle_7")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimSetValue",
+                  "parameter": {
+                    "target": [ "#s-Input_2" ],
+                    "value": {
+                      "datatype": "property",
+                      "target": "#s-Rectangle_7",
+                      "property": "jimGetValue"
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimHide",
+                  "parameter": {
+                    "target": [ "#s-Options_1" ],
+                    "effect": {
+                      "type": "slide",
+                      "easing": "swing",
+                      "duration": 500,
+                      "direction": "up"
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimRotate",
+                  "parameter": {
+                    "target": [ "#s-arrow_1" ],
+                    "angle": {
+                      "type": "rotateby",
+                      "value": "180"
+                    },
+                    "effect": {
+                      "type": "none",
+                      "easing": "swing",
+                      "duration": 400
+                    }
+                  },
+                  "exectype": "parallel",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Input_2")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimShow",
+                  "parameter": {
+                    "target": [ "#s-Options_1" ],
+                    "effect": {
+                      "type": "slide",
+                      "easing": "swing",
+                      "duration": 400,
+                      "direction": "up"
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimRotate",
+                  "parameter": {
+                    "target": [ "#s-arrow_1" ],
+                    "angle": {
+                      "type": "rotateby",
+                      "value": "180"
+                    },
+                    "effect": {
+                      "type": "none",
+                      "easing": "swing",
+                      "duration": 400
+                    }
+                  },
+                  "exectype": "parallel",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-arrow_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimShow",
+                  "parameter": {
+                    "target": [ "#s-Options_1" ],
+                    "effect": {
+                      "type": "slide",
+                      "easing": "swing",
+                      "duration": 400,
+                      "direction": "up"
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimRotate",
+                  "parameter": {
+                    "target": [ "#s-arrow_1" ],
+                    "angle": {
+                      "type": "rotateby",
+                      "value": "180"
+                    },
+                    "effect": {
+                      "type": "none",
+                      "easing": "swing",
+                      "duration": 400
+                    }
+                  },
+                  "exectype": "parallel",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Rectangle_2")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimSetValue",
+                  "parameter": {
+                    "target": [ "#s-Input_1" ],
+                    "value": {
+                      "datatype": "property",
+                      "target": "#s-Rectangle_2",
+                      "property": "jimGetValue"
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimHide",
+                  "parameter": {
+                    "target": [ "#s-Options" ],
+                    "effect": {
+                      "type": "slide",
+                      "easing": "swing",
+                      "duration": 500,
+                      "direction": "up"
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimRotate",
+                  "parameter": {
+                    "target": [ "#s-arrow" ],
+                    "angle": {
+                      "type": "rotateby",
+                      "value": "180"
+                    },
+                    "effect": {
+                      "type": "none",
+                      "easing": "swing",
+                      "duration": 400
+                    }
+                  },
+                  "exectype": "parallel",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Rectangle_3")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimSetValue",
+                  "parameter": {
+                    "target": [ "#s-Input_1" ],
+                    "value": {
+                      "datatype": "property",
+                      "target": "#s-Rectangle_3",
+                      "property": "jimGetValue"
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimHide",
+                  "parameter": {
+                    "target": [ "#s-Options" ],
+                    "effect": {
+                      "type": "slide",
+                      "easing": "swing",
+                      "duration": 500,
+                      "direction": "up"
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimRotate",
+                  "parameter": {
+                    "target": [ "#s-arrow" ],
+                    "angle": {
+                      "type": "rotateby",
+                      "value": "180"
+                    },
+                    "effect": {
+                      "type": "none",
+                      "easing": "swing",
+                      "duration": 400
+                    }
+                  },
+                  "exectype": "parallel",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Rectangle_4")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimSetValue",
+                  "parameter": {
+                    "target": [ "#s-Input_1" ],
+                    "value": {
+                      "datatype": "property",
+                      "target": "#s-Rectangle_4",
+                      "property": "jimGetValue"
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimHide",
+                  "parameter": {
+                    "target": [ "#s-Options" ],
+                    "effect": {
+                      "type": "slide",
+                      "easing": "swing",
+                      "duration": 500,
+                      "direction": "up"
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimRotate",
+                  "parameter": {
+                    "target": [ "#s-arrow" ],
+                    "angle": {
+                      "type": "rotateby",
+                      "value": "180"
+                    },
+                    "effect": {
+                      "type": "none",
+                      "easing": "swing",
+                      "duration": 400
+                    }
+                  },
+                  "exectype": "parallel",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Input_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimShow",
+                  "parameter": {
+                    "target": [ "#s-Options" ],
+                    "effect": {
+                      "type": "slide",
+                      "easing": "swing",
+                      "duration": 400,
+                      "direction": "up"
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimRotate",
+                  "parameter": {
+                    "target": [ "#s-arrow" ],
+                    "angle": {
+                      "type": "rotateby",
+                      "value": "180"
+                    },
+                    "effect": {
+                      "type": "none",
+                      "easing": "swing",
+                      "duration": 400
+                    }
+                  },
+                  "exectype": "parallel",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-arrow")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimShow",
+                  "parameter": {
+                    "target": [ "#s-Options" ],
+                    "effect": {
+                      "type": "slide",
+                      "easing": "swing",
+                      "duration": 400,
+                      "direction": "up"
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimRotate",
+                  "parameter": {
+                    "target": [ "#s-arrow" ],
+                    "angle": {
+                      "type": "rotateby",
+                      "value": "180"
+                    },
+                    "effect": {
+                      "type": "none",
+                      "easing": "swing",
+                      "duration": 400
+                    }
+                  },
+                  "exectype": "parallel",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Rectangle_8")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/11ff575b-cdf7-49f6-9ad0-085b905959f2"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  });
